9a7ff3738d141aec013e91d350a4cd79f9ad8925
Deepak Agarwal <adeepak269@gmail.com>
2018-05-24 11:07:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,23 +1,18 @@
 'use strict';
 
-const express = require('express');
-const socketIO = require('socket.io');
-const path = require('path');
-const PORT = process.env.PORT || 3000;
-const INDEX = path.join(__dirname, 'index.html');
-const ROBOT = path.join(__dirname, 'robot.html');
-const server = express()
-  .use((req, res) => res.sendFile(INDEX) )
-  .use((req, res) => res.sendFile(ROBOT) )
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
-const io = socketIO(server);
+server.listen(80);
 
-io.on('connection', (socket) => {
-  console.log('Client connected');
-  socket.on('disconnect', () => console.log('Client disconnected'));
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/index.html');
 });
 
-//setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
-
-setInterval(() => io.emit('time', 500), 1000);
+io.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
