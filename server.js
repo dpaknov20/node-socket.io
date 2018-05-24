be6cecb731005304dbaceb0b56e1aa8fb398d87a
Deepak Agarwal <adeepak269@gmail.com>
2018-05-24 11:38:11 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,12 @@ const server = express()
   .use((req, res) => res.sendFile(INDEX) )
 
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
-
+server.get('/robot', function (req, res) {
+  res.send('Hello World!')
+})
+server.get('/mainfile', function (req, res) {
+   res.sendFile(path.join(__dirname, 'index.html'));
+})
 const io = socketIO(server);
 
 io.on('connection', (socket) => {
